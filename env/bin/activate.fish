7464b32a7c274bbbc2df01345ad022f51b9b3691
Julia Mathias <juliamss@gmail.com>
2021-01-19 21:30:46 -0300

    Created requirements file

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/popcandy/app/env"
+set -gx VIRTUAL_ENV "/home/popcandy/code/JuliaMathias/first-flask-app/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
